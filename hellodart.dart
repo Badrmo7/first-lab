cf8aa23bf4ae78a1b3a8fddc717d63e75c62ad75
441460347 <113971421+Badrmo7@users.noreply.github.com>
2022-10-18 10:21:46 +0300

    Create hellodart.dart
    first lab

---
+++ hellodart.dart
@@ -0,0 +1,17 @@
+void main() {
+  print('Hello Class!');
+  String nk = "Hello Class Students";
+  print(nk);
+
+  bool x;
+  x = 1000 > 200;
+  print(x);
+  
+  String name = 'Shaqra';
+  if (name.length != 0){
+  print('String not empty!');
+  }
+  else{
+  print('String is empty!');
+}
+  }
