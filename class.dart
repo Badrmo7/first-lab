a29d6e8be1434e524e82527331cb852f59272b83
441460347 <113971421+Badrmo7@users.noreply.github.com>
2022-10-18 10:51:37 +0300

    Create class.dart

---
+++ class.dart
@@ -0,0 +1,15 @@
+class student {
+   dynamic name = "Badr";
+   String university = "Shaqra";
+  
+   void printName() {
+     print(name);
+   }
+}
+
+void main() {
+  student s1 = new student();
+  s1.printName;
+  print(s1.university);
+}
+  
